d0d7d7c70d321238e8170cf1b1e84a856444821a
seowon0825 <bsw0825@naver.com>
2024-07-05 15:16:31 +0900

    Repository 임포트

--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/repository/TicketingRepository.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/repository/TicketingRepository.java
@@ -2,6 +2,8 @@ package com.example.POPCornPickApi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.POPCornPickApi.entity.Ticketing;
+
 public interface TicketingRepository extends JpaRepository<Ticketing, Long>{
 
 }
--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/repository/UnknownMemberRepository.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/repository/UnknownMemberRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UnknownMemberRepository extends JpaRepository<UnknownMemberRepository, String>{
 
 	
+	
 }

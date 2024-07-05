20c1be229499bd4745897cc399309e66dacefcc2
Jeong HanYeon <gksdusdl5@gmail.com>
2024-07-05 14:56:50 +0900

    담당 Repository 쿼리문 작성

--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/repository/NoticeRepository.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/repository/NoticeRepository.java
@@ -1,9 +1,11 @@
 package com.example.POPCornPickApi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.POPCornPickApi.entity.Notice;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long>{
-
+	public Notice findByNoticeNo(Long noticeNo);
 }

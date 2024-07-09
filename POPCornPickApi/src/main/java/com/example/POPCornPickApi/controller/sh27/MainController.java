85eb8323c5b731313d02a41f92607c6fb8df4588
ParkSaehee <saehee0427@gmail.com>
2024-07-09 12:06:14 +0900

    movieName entity 생성

--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/controller/sh27/MainController.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/controller/sh27/MainController.java
@@ -1,11 +1,29 @@
 package com.example.POPCornPickApi.controller.sh27;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.POPCornPickApi.service.MovieNameService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/v1/main")
+@CrossOrigin(origins = "http://localhost:8080")
 public class MainController {
 
-	
+    @Autowired
+    private MovieNameService movieNameService;
+
+    @GetMapping("/movies")
+    public String getAllMovies(HttpServletRequest request) {
+        List<String> movies = movieNameService.getAllMoviesWithDetails();
+        request.setAttribute("movies", movies);
+        return "movies";
+    }
 }

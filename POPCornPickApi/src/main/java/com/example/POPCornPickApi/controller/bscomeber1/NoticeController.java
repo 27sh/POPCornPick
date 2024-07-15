611bc773a10d7299b7e5a3eadbb29f715f8066d5
bscomebr1 <bscomebr1@gmail.com>
2024-07-15 18:44:30 +0900

    07-15

--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/controller/bscomeber1/NoticeController.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/controller/bscomeber1/NoticeController.java
@@ -1,34 +1,79 @@
 package com.example.POPCornPickApi.controller.bscomeber1;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.POPCornPickApi.dto.NoticeListDto;
 import com.example.POPCornPickApi.entity.Notice;
 import com.example.POPCornPickApi.service.NoticeService;
 
 @RestController
-@CrossOrigin("*")
+//@CrossOrigin("*")
 @RequestMapping("/api/v1/admin")
 public class NoticeController {
 
+	
+	@Value("${spring.servlet.multipart.location}")
+	private String uploadDir;
+	
 	@Autowired
 	private NoticeService noticeService;
 	
+	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@PostMapping("/announcement")
-	public ResponseEntity<Notice> WriteNotice(@RequestBody Notice notice) {
+	public @ResponseBody String writeNotice(@RequestBody NoticeListDto noticedto) {
 		
-		return ResponseEntity.ok(noticeService.saveNotice(notice));
+		Notice notice = new Notice();
+		String originName = noticedto.getFileNema();
+		
+		notice.setNoticeTitle(noticedto.getNoticeTitle());
+		notice.setNoticeCategory(noticedto.getNoticeCategory());
+		notice.setNoticeContent(noticedto.getNoticeContent());
+		
+		notice.setNoticeFile(originName);
+		String newName = UUID.randomUUID().toString() + "_" + originName;
+		File file = new File(uploadDir, newName);
+		
+		try {
+			noticedto.getImgfile().transferTo(file);
+			System.out.println("파일업로드 완료");
+			boolean result = noticeService.registNotice(notice);
+			if(result) {
+				System.out.println("noticeForm File true");
+				return "등록되었습니다.";
+			}else {
+				System.out.println("noticeForm File false");
+				return "등록이 안됩니다";
+			}
+			
+		}catch(IllegalStateException | IOException e){
+			e.printStackTrace();
+			return "파일 저장중 에러 발 생 : " + e.getMessage();
+		}
+
 	}
+	
+//	public ResponseEntity<Notice> WriteNotice(@RequestBody Notice notice) {
+//		
+//		return ResponseEntity.ok(noticeService.saveNotice(notice));
+//	}
 	
 	@GetMapping("/announcementList")
 	public List<Notice> getNoticeList(){
@@ -45,5 +90,34 @@ public class NoticeController {
 		
 		return ResponseEntity.status(HttpStatus.OK).body(notice);
 	}
+	
+	//noticeDetail
+	@PutMapping("/announcementput")
+	public ResponseEntity<Notice> putNoticeDetail(@RequestBody Notice modifynotice){
+		
+		Long noticeNo = 1L;
+		Notice notice = noticeService.getnoticeDetial(noticeNo);
+		
+		System.out.println(notice);
+		System.out.println("모디파이노티스" + modifynotice);
+		 if (notice != null) {
+		        System.out.println("수정을 시작합니다");
+		        notice.setNoticeContent(modifynotice.getNoticeContent());
+		        notice.setNoticeTitle(modifynotice.getNoticeTitle());
+
+		        if (modifynotice.getNoticeFile() != null && !modifynotice.getNoticeFile().isEmpty()) {
+		            notice.setNoticeFile(modifynotice.getNoticeFile());
+		        }
+
+		        notice = noticeService.modifynotice(notice);
+		        return ResponseEntity.ok().body(notice);
+		    } else {
+		        // 처리할 내용이 없다면 적절한 응답을 반환합니다.
+		        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		    }
+		
+	}
+	
+	
 	
 }

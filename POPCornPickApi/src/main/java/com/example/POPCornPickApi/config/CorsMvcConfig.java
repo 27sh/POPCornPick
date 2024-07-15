fb75cd8b9d69ea2c5179e43246698fe9e76d2659
Jeong HanYeon <gksdusdl5@gmail.com>
2024-07-15 15:24:07 +0900

    OAuth2 제외 토큰까지만 구현

--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/config/CorsMvcConfig.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/config/CorsMvcConfig.java
@@ -11,9 +11,16 @@ public class CorsMvcConfig implements WebMvcConfigurer{
 	public void addCorsMappings(CorsRegistry corsRegistry) {
 		
 		corsRegistry.addMapping("/**")
-        			.exposedHeaders("Set-Cookie")
-        			.allowedOrigins("http://localhost:8080");
+        			//.exposedHeaders("Set-Cookie")
+        			.allowedOriginPatterns("http://localhost:8080")
+        			//.allowedOrigins("http://localhost:8080")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("*")
+					.allowCredentials(true);
 		
 	}
+	
+	
+	
 	
 }
--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/dto/OAuth2Response.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/dto/OAuth2Response.java
@@ -1,21 +1,21 @@
-package com.example.POPCornPickApi.dto;
-
-import java.util.Map;
-
-public interface OAuth2Response {
-	
-	//제공자 (Ex. naver, google, ...)
-    String getProvider();
-    //제공자에서 발급해주는 아이디(번호)
-    String getProviderId();
-    //이메일
-    String getEmail();
-    //사용자 실명 (설정한 이름)
-    String getName();
-    // 확인용
-    Map<String, Object> getCheck();
-//    // 사용자 폰 번호
-//    String getPhoneNumber();
-//    // 사용자 성별
-//    String getGender();
-}
+//package com.example.POPCornPickApi.dto;
+//
+//import java.util.Map;
+//
+//public interface OAuth2Response {
+//	
+//	//제공자 (Ex. naver, google, ...)
+//    String getProvider();
+//    //제공자에서 발급해주는 아이디(번호)
+//    String getProviderId();
+//    //이메일
+//    String getEmail();
+//    //사용자 실명 (설정한 이름)
+//    String getName();
+//    // 확인용
+//    Map<String, Object> getCheck();
+////    // 사용자 폰 번호
+////    String getPhoneNumber();
+////    // 사용자 성별
+////    String getGender();
+//}

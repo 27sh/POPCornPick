8e26299796c11adad199f2f4b840b58f16bab6f4
Kim ye young <kimyyoung1018@gmail.com>
2024-07-23 15:45:21 +0900

    schedule

--- POPCornPickApi/src/main/java/com/example/POPCornPickApi/controller/kyy1018/ScheduleController.java
+++ POPCornPickApi/src/main/java/com/example/POPCornPickApi/controller/kyy1018/ScheduleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -102,7 +103,7 @@ public class ScheduleController {
 
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-
+	
 
 	// 영화관 별 세부 상영시간표 수정
 	@PutMapping("/{roomNo}")
